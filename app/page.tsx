4ac5b07cd57763c60199e4feb8dd7e0a8a974fcf
Areg Abrahamyan <areg.abrahamyan3@gmail.com>
2024-08-20 12:17:48 -0800

    Fixed Stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 // import { CustomKanban } from '@/components/CustomKanban'
 
+import { BackgroundBeams } from '@/components/BackgroundBeams'
+
 export default function Home() {
   return (
-    // <CustomKanban />
+    <>
+    <BackgroundBeams />
     <p>Hello World hello</p> 
+    </>
   );
 }
