7a0c14d312ad5062079868f0d66121959b4b25d2
Areg Abrahamyan <areg.abrahamyan3@gmail.com>
2024-08-20 12:25:25 -0800

    Fixed stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-// import { CustomKanban } from '@/components/CustomKanban'
 
 import { BackgroundBeams } from '@/components/BackgroundBeams'
+import { FloatingNav } from '@/components/ui/FloatingNav'
 
 export default function Home() {
   return (
     <>
     <BackgroundBeams />
+    <FloatingNav />
     <p>Hello World hello</p> 
     </>
   );
