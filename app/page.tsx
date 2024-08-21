cb2ddf4b0608863a8f34b5cdf405f210b927e25b
Areg Abrahamyan <areg.abrahamyan3@gmail.com>
2024-08-21 00:41:34 -0800

    Fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,7 @@
-import { BackgroundBeams } from '@/components/BackgroundBeams'
-import BasicSkills from '@/components/BasicSkills'
-import Clients from '@/components/Clients'
-import Hero from '@/components/Hero'
-import RecentProjects from '@/components/RecentProjects'
-import Footer from '@/components/Footer'
-import { FloatingNav } from '@/components/ui/FloatingNav'
+import { CustomKanban } from '@/components/CustomKanban'
 
 export default function Home() {
-	return (
-		<main className='relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 z-0'>
-			<div className='max-w-7xl w-full'>
-				<BackgroundBeams />
-				<FloatingNav />
-				<Hero />
-				<BasicSkills />
-				<RecentProjects />
-				<Clients />
-				<Footer />
-			</div>
-		</main>
-	)
+  return (
+    <CustomKanban />
+  );
 }
