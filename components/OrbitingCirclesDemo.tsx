e9f91c2f1cc46007a0777e781e6aebac0fe9202b
Areg Abrahamyan <areg.abrahamyan3@gmail.com>
2024-08-20 23:40:16 -0800

    Fixed Stuff

--- components/OrbitingCirclesDemo.tsx
+++ components/OrbitingCirclesDemo.tsx
@@ -8,29 +8,11 @@ import { HoverBorderGradient } from './ui/hover-border-gradient'
 import { Link, BrowserRouter as Router } from 'react-router-dom'
 
 export function OrbitingCirclesDemo() {
-	const [notification, setNotification] = useState<string | null>(null)
-	const email = 'areg.abrahamyan3@gmail.com'
-
-	const copyToClipboard = async () => {
-		try {
-			await navigator.clipboard.writeText(email)
-			setNotification('Email copied to clipboard!')
-			setTimeout(() => setNotification(null), 3000)
-		} catch (err) {
-			console.error('Failed to copy!', err)
-			setNotification('Failed to copy email.')
-			setTimeout(() => setNotification(null), 3000)
-		}
-	}
-
-	const deleteNotification = () => {
-		setNotification(null)
-	}
 
 	return (
 		<div className='relative top-16 flex h-[450px] w-full flex-col items-center justify-center overflow-hidden rounded-lg'>
 			<Router>
-				<Link to='mailto:areg.abrahamyan3@gmail.com'>
+				<Link to=''>
 					<HoverBorderGradient className='p-2 px-3 hover-effect'>
 						<li
 							className='relative overflow-hidden block text-center grow p-0 text-indigo-300'
