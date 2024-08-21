4017e0f47897b6c0f93d0436af602e62300b1a04
Areg Abrahamyan <areg.abrahamyan3@gmail.com>
2024-08-20 23:44:09 -0800

    Fixed Stuff

--- components/OrbitingCirclesDemo.tsx
+++ components/OrbitingCirclesDemo.tsx
@@ -30,7 +30,7 @@ export function OrbitingCirclesDemo() {
 	return (
 		<div className='relative top-16 flex h-[450px] w-full flex-col items-center justify-center overflow-hidden rounded-lg'>
 			<Router>
-				<Link to='https://github.com/vercel/next.js/discussions/53972'>
+				<Link to='mailto:areg.abrahamyan3@gmail.com'>
 					<HoverBorderGradient className='p-2 px-3 hover-effect'>
 						<li
 							className='relative overflow-hidden block text-center grow p-0 text-indigo-300'
