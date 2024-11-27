a9de10419ea4e96f86d5b2fac38e861a87ba9378
Areg Abrahamyan <areg.abrahamyan3@gmail.com>
2024-11-27 12:13:37 -0900

    first

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ const HomePage = async () => {
     <div className="">
       {/* <Page /> */}
       <Slider />
-      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      {/* <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Featured Products</h1>
         <Suspense fallback={<Skeleton />}>
           <ProductList
@@ -43,7 +43,7 @@ const HomePage = async () => {
             limit={4}
           />
         </Suspense>
-      </div>
+      </div> */}
       {/* <div className="mt-24">
         <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
           Categories
