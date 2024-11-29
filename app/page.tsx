3fdf42c66fd9bef6c750868506ad4b2589f6e8f9
Areg Abrahamyan <areg.abrahamyan3@gmail.com>
2024-11-29 11:22:15 -0900

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ const HomePage = async () => {
   return (
     <div className="">
       {/* <Page /> */}
-      {/* <Slider /> */}
+      <Slider />
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Featured Products</h1>
         {/* <Suspense fallback={<Skeleton />}>
